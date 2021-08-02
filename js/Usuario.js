eb04acfc6902c6a88c341dbe2afae01c8041e522
Enrique Alberto González Galaviz <301910232@ute.edu.mx>
2021-08-01 23:21:11 -0500

    nuevos cambio, registrar

--- js/Usuario.js
+++ js/Usuario.js
@@ -1,8 +1,10 @@
+const form  = document.getElementById('form')
 const empresa = document.getElementById("empresa_nombre");
 const password = document.getElementById("password_empresa");
 const btn_registro = document.getElementById("btn_registro");
+const parrafo = document.getElementById("warnings");
 
-
+// alerta
 btn_registro.addEventListener('click', ()=>{
 
     if (empresa.value === "" || password.value === "") {
@@ -10,7 +12,33 @@ btn_registro.addEventListener('click', ()=>{
         return;
     }
 
-    //hacer le peticion a la API para crear una cuenta
+// // localStorage
+// form.addEventListener('submit', function(event){
+//     event.preventDefault();
+//     let users = Array(
+//         {
+//             empresa: empresa_nombre.value,
+//             password: password_empresa.value
+//         }
+//     )
+// });
 
-})
+
+        // form.addEventListener("submit", e=>{
+        //     e.preventDefault()
+        //         let warnings =""
+        //         if(empresa.value.length<6){
+        //             warnings += 'El nombre no es valido'
+        //         }
+        //         if(password.value.length<8){
+        //             warnings += 'campos incompletos'
+        //             entrar = true
+        //         }
+
+        //     });    
+    // localStorage.setItem('us', JSON.stringify(users));
+    // location.href="../html/lista.html"
+        //hacer le peticion a la API para crear una cuenta
+            
+        })
 
---
+++ js/registrar.js
@@ -0,0 +1,6 @@
+const form  = document.getElementById('form')
+const empresa = document.getElementById("empresa_nombre");
+const password = document.getElementById("password_empresa");
+const btn_registro = document.getElementById("btn_registro");
+const corre = document.getElementById("corre");
+
